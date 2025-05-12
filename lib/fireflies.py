2419fdf2af3eee332df1fb5213f8c9a8ecc4795d
George (Gosha) Ginzburg <ginzburggeorgy@gmail.com>
2025-05-13 01:27:52 +0300

    refactor Firefly class to include ID attribute and update phase threshold; add function to create d-regular network

--- lib/fireflies.py
+++ lib/fireflies.py
@@ -1,23 +1,25 @@
 import numpy as np
+import networkx as nx
 from typing import Callable, List
 
 
 class Firefly:
     DEFAULT_RESET_STRENGTH = 0.1
     DEFAULT_ANGULAR_FREQ = 2 * np.pi  # 1Hz natural frequency
-    PHASE_THRESHOLD = 2 * np.pi  # When phase is >= PHASE_THRESHOLD, firefly is considered to be flashing
+    PHASE_THRESHOLD = 1.0  # When phase is >= PHASE_THRESHOLD, firefly is considered to be flashing
 
     class State:
         FLASHING = 1
         WAITING = 0
 
-    def __init__(self, phase: float, x: float, y: float,
+    def __init__(self, id: int, phase: float,
                  neighbours: List['Firefly'],
                  freq: float = DEFAULT_ANGULAR_FREQ,
                  phase_threshold: float = PHASE_THRESHOLD):
         """
         Base firefly class with core phase advancement logic
         """
+        self.id = id
         self.phase = phase
         self.neighbours = neighbours
         self.freq = freq
@@ -37,3 +39,15 @@ class Firefly:
     def reset(self):
         self.phase = 0
         self.state = self.State.WAITING
+
+
+def create_d_regular_network(phases: np.array, degree, seed=None) -> nx.Graph:
+    """
+    Create a d-regular random graph on nodes_num nodes. We use a regular graph so that
+    each firefly has the same number of neighbours, and as such we don't have to think about
+    topology of the network.
+    """
+    G = nx.random_regular_graph(degree, len(phases), seed=seed)
+    firefly_dict = {node: Firefly(node, phase) for (node, phase) in zip(G.nodes(), phases)}
+    nx.set_node_attributes(G, firefly_dict, 'firefly')
+    return G
